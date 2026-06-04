dbbde7ca76a5cfbc282741fd7b8aea24e1d338fd
GeovaneFerreira26 <geovane.paixao@ba.estudante.senai.br>
2026-06-04 18:14:06 -0300

    atualização formulario

--- atividade_spa/src/Homenagens/index.jsx
+++ atividade_spa/src/Homenagens/index.jsx
@@ -10,6 +10,7 @@ export default function Homenagens() {
   const [mensagens, setMensagens] = useState([
     { id: 1, nome: 'Mãe', texto: 'Você é o nosso maior orgulho, Geovane! Te amamos muito.' },
     { id: 2, nome: 'Pai', texto: 'Ver suas conquistas de perto enche meu coração de alegria. Continue sempre focado!' }
+    { id: 2, nome: 'Juli', texto: 'Aqui é a minha família favorita! Amo vocês! ❤️' }
   ]);
 
   // Atualiza os valores dos inputs enquanto o usuário digita
